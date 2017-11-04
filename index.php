2da62d755b31fbf3db5a3abb3454ac46514ad5bb
braskinha <contasklauw@gmail.com>
2017-11-04 12:32:26 -0200

    modificação

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PrimeiroSite</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Roboto+Condensed" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Olá</h1>
+    <img src="http://lorempixel.com/400/200/">
+
+</body>
+
+</html>
